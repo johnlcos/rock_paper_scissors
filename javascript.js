2911a8f5b1ff7ad9782d9ce66ff2cbc29bdc8c2c
johnlcos <113793649+johnlcos@users.noreply.github.com>
2023-06-07 12:51:36 -0400

    add functionality to UI and basic gameflow

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
 //initialize needed variables
 let playerSelection;
-let playerScore = 0;
 let computerSelection;
-let computerScore = 0;
 let roundResult;
 let playOptions = ['rock','paper','scissors'];
+
+//initialize players choice buttons
+const btnRock = document.querySelector('.btn-rock');
+const btnPaper = document.querySelector('.btn-paper');
+const btnScissors = document.querySelector('.btn-scissors');
+
+//initialize player and computer scores
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+
+//initialize game updates
+const gameUpdates = document.querySelector('.game-updates');
 
 //function the randomly selects the computers choice
 function getComputerChoice() {
@@ -14,71 +24,53 @@ function getComputerChoice() {
     return playOptions[randomIndex];
 }
 
-//function to prompt the user for their choice
-function getPlayerChoice() {
-    //initialize user choice to something not possible
-    let userInput = "";
-    //while user inputs anything but rock paper or scissors, keep prompting
-    while (!playOptions.includes(userInput)) {
-        userInput = prompt('Rock, Paper or Scissors?:').toLocaleLowerCase();
-    }
-    return userInput;
-}
-
 //function to play a round
 function playRound(computerSelection, playerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
     //if the player picks the same as the computer its a tie
     if (playerSelection == computerSelection) {
-        return `You picked ${playerSelection} and the computer picked ${computerSelection}, it's a tie!`;
+        gameUpdates.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}, it's a tie!`;
     }
     //win conditions for the player
     else if ((playerSelection == 'rock' && computerSelection == 'scissors')||
     (playerSelection == 'paper' && computerSelection == 'rock')||
     (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        playerScore = ++playerScore;
-        return `You picked ${playerSelection} and the computer picked ${computerSelection}, you win!`;
+        let counter = parseInt(playerScore.textContent);
+        counter++;
+        playerScore.textContent = counter;
+        gameUpdates.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}, you win!`;
     }
     //if its not a tie and the player didnt win, then they lose
     else {
-        computerScore = ++computerScore;
-        return `You picked ${playerSelection} and the computer picked ${computerSelection}, you lose!`;
+        let counter = parseInt(computerScore.textContent);
+        counter++;
+        computerScore.textContent = counter;
+        gameUpdates.textContent = `You picked ${playerSelection} and the computer picked ${computerSelection}, you lose!`;
     }
-   
+    gameOver()
 }
 
-//function to play a game
-function game() {
-    //start game counter at 0, run for 5 rounds
-    let gameCounter = 0;
-    while (gameCounter < 5) {
-        //get computer choice
-        computerSelection = getComputerChoice();
-        console.log(computerSelection);
-        //get player choice
-        playerSelection = getPlayerChoice();
-        console.log(playerSelection);
-        //check for round winner
-        roundResult = playRound(computerSelection, playerSelection);
-        console.log(roundResult);
-        console.log(playerScore);
-        console.log(computerScore);
-        //increase round counter
-        gameCounter = ++gameCounter;
+//add event listeners to each button and play a round
+btnRock.addEventListener('click', () => {playRound(getComputerChoice(),'rock')});
+btnPaper.addEventListener('click', () => {playRound(getComputerChoice(),'paper')});
+btnScissors.addEventListener('click', () => {playRound(getComputerChoice(),'scissors')});
+
+//function to check if game over
+function gameOver() {   
+    //if player or computer has won 5 rounds, report and start new game   
+    if (parseInt(playerScore.textContent) == 5) {
+        gameUpdates.textContent = `You won ${playerScore.textContent} rounds and the computer won ${computerScore.textContent}, you win the game! :)`;
+        computerScore.textContent = 0;
+        playerScore.textContent = 0;
     }
-    //after 5 rounds, check scores and report the winner
-    if (playerScore == computerScore) {
-        return `You won ${playerScore} rounds and the computer won ${computerScore}, the game is a ties!`;
-    }   
-    else if (playerScore > computerScore) {
-        return `You won ${playerScore} rounds and the computer won ${computerScore}, you win the game! :)`;
-    }
-    else if (playerScore < computerScore) {
-        return `You won ${playerScore} rounds and the computer won ${computerScore}, you lose the game! :(`;
+    else if (parseInt(computerScore.textContent) == 5) {
+        gameUpdates.textContent = `You won ${playerScore.textContent} rounds and the computer won ${computerScore.textContent}, you lose the game! :(`;
+        computerScore.textContent = 0;
+        playerScore.textContent = 0;
     }
 }
 
-let gameResult = game()
-console.log(gameResult)
 
 
 
